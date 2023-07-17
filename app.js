25a2d0514ded4b89cc27aa12bcbcafcb344d6ffe
Anna <thoria@yandex.ru>
2023-07-17 03:55:02 +0300

    hotfix: исправить логику обработки неправильного пути

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-const router = require('express').Router();
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
@@ -25,7 +24,7 @@ app.use((req, res, next) => {
 });
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
-router.all('*', (req, res) => {
+app.use('*', (req, res) => {
   res.status(404).send({ message: 'Неверный адрес запроса' });
 });
 

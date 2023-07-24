f73276b49bad958762ef7f3b28041c4c8fadcb21
Anna <thoria@yandex.ru>
2023-07-24 08:45:10 +0300

    hotfix: правки после ревью

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,6 +1,6 @@
 const errors = (err, req, res, next) => {
   if (!err.statusCode) {
-    res.status(500).send({ message: 'Ошибка на сервере' });
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
   } else {
     res.status(err.statusCode).send({ message: err.message });
   }
